22ea9782941df9285ef13ec0dd5d930ec90b95d0
Alicia <alicia.sudlerd@ut.ee>
2023-10-04 10:06:50 +0300

    add camera into the build file

--- cpp/CubeChopperCPP/build.sh
+++ cpp/CubeChopperCPP/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ -o CubeChopperCPP main.cpp shader_util.cpp -I./include -L/lib -lglfw -lGLEW -lGL
+g++ -o CubeChopperCPP Camera.cpp main.cpp shader_util.cpp -I./include -L/lib -lglfw -lGLEW -lGL
 ./CubeChopperCPP
