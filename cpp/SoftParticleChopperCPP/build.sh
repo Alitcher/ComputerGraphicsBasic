f68ca183067c996dedf55384bb399d427ffe8a25
Alicia <alicia.sudlerd@ut.ee>
2023-10-18 17:29:55 +0300

    add build file for linux

---
+++ cpp/SoftParticleChopperCPP/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Compile individual source files into object files
+g++ -c src/geometry.cpp -Iinclude -o obj/src/geometry.o
+g++ -c src/main.cpp -Iinclude -o obj/src/main.o
+g++ -c src/shader_util.cpp -Iinclude -o obj/src/shader_util.o
+g++ -c src/texture_util.cpp -Iinclude -o obj/src/texture_util.o
+
+# Link object files to produce the final executable
+g++ obj/src/*.o -lglfw -lGLEW -lIL -lILU -lILUT -lGL -lX11 -lpthread -lXrandr -lXi -o bin/softParticleChopper
+
+echo "Build completed!"
